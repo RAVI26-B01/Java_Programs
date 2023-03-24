9f8afd8493d7365a511824747d266c98eb0177da
ravi <ravi@chegusinfotech.com>
2023-03-24 18:22:56 +0530

    workbook problem pending

---
+++ practice/WorkBookProblem.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class WorkBookProblem {
+
+	public static void main(String[] args) {
+		int chapter = 5;
+		int k = 3;  //problems in one page
+		int[] charpterArr = {4, 2, 6, 1, 10}; // problems in each chapter
+	}
+
+}
