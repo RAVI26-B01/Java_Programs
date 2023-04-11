c2bd14b6ab9d7162dbbdfb9fb151d626adb0d8b8
Ravi <ravi26.b01@gamil.com>
2023-04-11 14:57:51 +0530

    Level-Order & Maximun-Depth of a Tree

--- binaryTree/BinaryTreeMainProgram.java
+++ binaryTree/BinaryTreeMainProgram.java
@@ -1,5 +1,8 @@
 package binaryTree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 
@@ -8,98 +11,149 @@ class Node{
 	Node left,right;
 }
 public class BinaryTreeMainProgram {
-
+	static int max_level=0;
+	static Node root ;
 	public static void main(String[] args) {
-		BinaryTree bt=new BinaryTree();
 		Scanner scan=new Scanner(System.in);
 		while(true) {
 			System.out.println("press 1 to InsertFront");
 			System.out.println("press 2 to Pre-Order");
 			System.out.println("press 3 to In-Order");
 			System.out.println("press 4 to Post-Order");
+			System.out.println("press 5 to Level-Order");
+			System.out.println("press 7 to Maximun-Depth");
+			System.out.println("press 10 to left view Elements");
+			System.out.println();
 			
 			System.out.println("enter your choice");
 			int choice=scan.nextInt();
 				switch(choice) {
-					case 1:bt.insert();break;
-					case 2:bt.pre_order();break;
-					case 3:bt.in_order();break;
-					case 4:bt.post_order();break;
+					case 1:insert();break;
+					case 2:preorderTraversal(root);break;
+					case 3:inorderTraversal(root);break;
+					case 4:postorderTraversal(root);break;
+					case 5:leverOrderTraversal(root);break;
+					case 7:maximunDepth(root);break;
+					case 10:leftView();break;
 				//	case 6:ll.search();break;
 					default :System.out.println("program terminated :-((");System.exit(0);
 				}
 			}
-		
 	}
-
-}
-
-class BinaryTree{
-	Node root;
-	public void insert() {
-		Scanner scan=new Scanner(System.in);
-		System.out.print("Enter item to insert :");
-		int item=scan.nextInt();
-		Node node=new Node();
-		node.left=null;
-		node.data=item;
-		node.right=null;
 		
-		Node current,previous;
-		if(root == null){
-			root=node;
-		}else{
-			previous=null;
-			current=root;
-			while(current!=null){
-				if(node.data<current.data){
-					previous=current;
-					current=current.left;
+
+		private static void insert() {
+			Scanner scan=new Scanner(System.in);
+			System.out.print("Enter item to insert :");
+			int item=scan.nextInt();
+			Node node=new Node();
+			node.left=null;
+			node.data=item;
+			node.right=null;
+			
+			Node current,previous;
+			if(root == null){
+				root=node;
+			}else{
+				previous=null;
+				current=root;
+				while(current!=null){
+					if(node.data<current.data){
+						previous=current;
+						current=current.left;
+					}
+					else{
+						previous=current;
+						current=current.right;
+					}
+				}
+				if(node.data<previous.data){
+					previous.left=node;
 				}
 				else{
-					previous=current;
-					current=current.right;
+					previous.right=node;
 				}
-			}
-			if(node.data<previous.data){
-				previous.left=node;
-			}
-			else{
-				previous.right=node;
 			}
 		}
 		
-	}
-	public void pre_order() {
-		preorderTraversal(root);
-	}
-	private void preorderTraversal(Node current) {
-		if(current!=null){
-			System.out.print(current.data+" ");
-			preorderTraversal(current.left);
-			preorderTraversal(current.right);
+		
+		private static  void leftView() {
+			max_level=0;
+			leftViewOfTree(root,1);
 		}
-	}
-	
-	public void in_order() {
-		inorderTraversal(root);
-	}
-	private void inorderTraversal(Node current) {
-		if(current!=null){
-			inorderTraversal(current.left);
-			System.out.print(current.data+" ");
-			inorderTraversal(current.right);
+		private static void leftViewOfTree(Node current, int level) {
+			
+			if(current==null)
+				return;
+			if(max_level < level){
+				System.out.print(current.data+" ");
+				max_level=level;
+			}
+			leftViewOfTree(current.left, level+1);
+			leftViewOfTree(current.right, level+1);
 		}
-	}
-	public void post_order() {
-		postorderTraversal(root);
-	}
-	private void postorderTraversal(Node current) {
-		if(current!=null){
-			postorderTraversal(current.left);
-			postorderTraversal(current.right);
-			System.out.print(current.data+" ");
+		
+		private static void preorderTraversal(Node current) {
+			if(current!=null){
+				System.out.print(current.data+" ");
+				preorderTraversal(current.left);
+				preorderTraversal(current.right);
+			}
 		}
-	}
-	
+		
+		
+		private static void inorderTraversal(Node current) {
+			if(current!=null){
+				inorderTraversal(current.left);
+				System.out.print(current.data+" ");
+				inorderTraversal(current.right);
+			}
+		}
+		
+		private static void postorderTraversal(Node current) {
+			if(current!=null){
+				postorderTraversal(current.left);
+				postorderTraversal(current.right);
+				System.out.print(current.data+" ");
+			}
+		}
+		
+		private static void leverOrderTraversal(Node current) {
+			Queue<Node> queue = new LinkedList<>();
+			List<List<Integer>> ans = new LinkedList<List<Integer>>();
+			if(current == null){
+				return;
+			}
+			queue.offer(current);
+			while(!queue.isEmpty()){
+				int levelNums = queue.size();
+				List<Integer> subList = new LinkedList<>();
+				for( int i=0 ;i<levelNums ; i++){
+					if(queue.peek().left!=null)
+						queue.offer(queue.peek().left);
+					if(queue.peek().right!=null)
+						queue.offer(queue.peek().right);
+					subList.add(queue.poll().data);
+					
+				}
+				ans.add(subList);
+			}
+			System.out.println(ans);
+			}
+		
+		
+
+		private static void maximunDepth(Node current) {
+			int max_depth = max_depth(current);
+			System.out.println(max_depth);
+		}
+
+
+		private static int max_depth(Node current) {
+			if(current==null)
+				return 0;
+			int l= max_depth(current.left);
+			int r=max_depth(current.right);
+			return 1+Math.max(l, r);
+		}
 }
