0574c8e150555278541259f266d2badadbd186b5
ravi <ravi@chegusinfotech.com>
2023-04-19 12:59:46 +0530

    tested

--- practice/MinimumSwapToMakeEqual.java
+++ practice/MinimumSwapToMakeEqual.java
@@ -13,7 +13,7 @@ public class MinimumSwapToMakeEqual {
 		int count=0;
 		for(int i = 0 ; i<s1.length() ; i++) {
 			if(s1.charAt(i) != s2.charAt(i))
-				count++;
+				count++;    
 		}
 		return count/2;
 	}
