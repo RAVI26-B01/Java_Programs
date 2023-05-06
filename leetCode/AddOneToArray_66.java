8d65608ccd27ed0f6ce0f6b39b551c4959d1c091
Ravi <ravi26.b01@gamil.com>
2023-05-06 11:59:34 +0530

    Add one to given Array

---
+++ leetCode/AddOneToArray_66.java
@@ -0,0 +1,32 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class AddOneToArray_66 {
+
+	public static void main(String[] args) {
+		int[] ar = {9};
+		int[] addOne = addOne(ar);
+		System.out.println(Arrays.toString(addOne));
+	}
+
+	private static int[] addOne(int[] ar) {
+		
+		int n = ar.length;
+		int carry=0;
+		for(int i=n-1 ; i>=0 ; i--){
+			if(ar[i]!=9){
+				ar[i]++;
+				return ar;
+			}else{
+				ar[i]=0;
+			}
+		}
+		
+		int[] newArr = new int[n+1];
+		newArr[0]=1;
+		return newArr;
+		
+	}
+
+}
