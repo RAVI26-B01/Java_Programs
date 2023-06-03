d14c94c24a07276adacf70d5e6e1813bbf70bfa2
ravi <ravi@chegusinfotech.com>
2023-06-03 12:36:30 +0530

    Delete btw zp & Don't add btw 6-7

---
+++ practice/DeleteBtwZP.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class DeleteBtwZP {
+
+	public static void main(String[] args) {
+
+		String s="zipxxzop";
+		System.out.println(s.replaceAll("z.p", "zp"));
+	}
+
+}
---
+++ practice/DontAddBtw67.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class DontAddBtw67 {
+
+	public static void main(String[] args) {
+
+		int[] ar = {3,4,1,9,6,4,2,7,8};
+		int dontAdd = dontAdd(ar);
+		System.out.println(dontAdd);
+	}
+
+	private static int dontAdd(int[] ar) {
+		int sum=0;
+		
+		for(int i=0 ; i< ar.length ; i++) {
+			if(ar[i]==6) {
+				i++;
+				while(ar[i]!=7)
+					i++;
+			}else {
+				sum+=ar[i];
+			}
+		}
+		return sum;
+	}
+
+}
